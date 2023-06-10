f5b427c5829748fcdf27dda81c481b6e4beeb2f1
Does-It-Matters <99038040+Does-It-Matters@users.noreply.github.com>
2023-06-10 20:17:44 +0900

    Update NavigationMyPage.js

--- shop/shop/src/components/NavigationMyPage.js
+++ shop/shop/src/components/NavigationMyPage.js
@@ -1,15 +1,21 @@
+import {Link} from 'react-router-dom';
+
 function NavigationMyPage(){
     return (
         <div className="nav">
             <div>
                 <h1>마이페이지</h1>
-                <ul>
-                    <li>주문 내역</li>
-                    <li>쿠폰함</li>
-                    <li>MY CLASS</li>
-                    <li>비행기 스탬프</li>
-                    <li>정보 수정</li>
-                    <li>회원 탈퇴</li>
+                <ul> 
+                    <Link to="orderHistory">
+                        <li>주문 내역</li>
+                    </Link>
+                        <li>MY CLASS</li>
+                        <li>비행기 스탬프</li>
+                    <Link to="infoUpdate">
+                        <li>정보 수정</li>
+                    </Link>
+                        <li>쿠폰함</li>
+                        <li>회원 탈퇴</li>
                 </ul>
             </div>
         </div>
